33d66f059eec51de21301d13ba2b4dbafca47567
Junxi <junxi@cxisoftware.com.au>
2025-04-09 19:16:30 +1000

    check formly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'fintech-form-buidler';
+
+  form = new FormGroup({});
+  model = { email: '' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'username',
+      type: 'input',
+      props: {
+        label: 'Username',
+        required: true,
+      },
+    },
+    {
+      key: 'password',
+      type: 'input',
+      props: {
+        label: 'Password',
+        required: true,
+        type: 'password',
+      },
+    },
+    {
+      key: 'confirm_password',
+      type: 'input',
+      props: {
+        label: 'Confirm Password',
+        required: true,
+        type: 'password',
+      },
+    },
+  ];
+
+  onSubmit(model: any) {
+    console.log(this.form, model);
+  }
 }
